e85c3126b183ca9278ae0a17b9a608d07c562610
Fred Stober <stober@cern.ch>
2016-03-25 15:42:26 +0100

    refactored utils

--- utils.py
+++ utils.py
@@ -1,3 +1,30 @@
+def expath(fn):
+	return os.path.realpath(os.path.expanduser(fn))
+
+def progress(iter, step = 1000, enable = True, speed = True):
+	if enable:
+		t_start = time.time()
+		try:
+			idx = 0
+			for idx, entry in enumerate(iter):
+				if idx % step == 0:
+					sys.stdout.write('\r%d' % idx)
+					if speed:
+						delta_t = float(time.time() - t_start)
+						if delta_t > 2:
+							sys.stdout.write(' %d/s' % (idx / delta_t))
+					sys.stdout.flush()
+				yield entry
+			sys.stdout.write('\n')
+			sys.stdout.flush()
+		except:
+			sys.stdout.write('\nError at %d\n' % idx)
+			sys.stdout.flush()
+			raise
+	else:
+		for entry in iter:
+			yield entry
+
 
 def get_cached(cache_file, fun, *args, **kwargs):
 	import json
